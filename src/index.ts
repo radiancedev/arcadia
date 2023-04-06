ba4528597aa19439d623d96df6951f5a5939327e
Irisu <nekomarisakirisame@gmail.com>
2023-04-06 10:10:30 -0400

    add exports

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * from "./Application";
+export * from "./routes/Route";
+export * from "./structures/Controller";
+export * from "./structures/ApplicationContext";
+export * from "./structures/types/Context";
