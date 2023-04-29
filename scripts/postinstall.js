342d3d345cd4aecdaf528998320b5e48a811bbfe
Irisu <nekomarisakirisame@gmail.com>
2023-04-29 06:33:13 -0400

    e

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,26 +1,32 @@
 const fs = require("fs");
 const path = require("path");
 
+const prismaORMCode = `import { PrismaClient } from "@prisma/client";
+
+export class ORM extends PrismaClient {
+    public omit<T, K extends keyof T>(obj: T | undefined, keys: K[]): Omit<T, K> {
+        if (!obj) {
+            return {} as Omit<T, K>;
+        }
+
+        const ret: Record<keyof T, any> | undefined = obj;
+        for (let key of Object.keys(obj)) {
+            if (keys.includes(key as K)) {
+                delete ret?.[key as keyof T];
+            }
+        }
+
+        return ret;
+    }
+}
+`;
+
 try {
     const _ = require("@prisma/client"); 
     
     // Prisma is installed, so we'll use it as an ORM.
-    // Replace the value in ORM.ts with the following:
-    let fileData = fs.readFileSync(path.join(__dirname, "../src/orm/ORM.ts"), "utf8");
-
-    // Add the following line to the top of the file:
-    // import { PrismaExtendedClient } from "./PrismaExtendedClient.orm.ts";
-    fileData = "import { PrismaExtendedClient } from \"./PrismaExtendedClient.orm\";\n" + fileData;
-
-    // Replace (this._orm = {}) with (this._orm = new PrismaExtendedClient())
-    fileData = fileData.replace("this._orm = {}", "this._orm = new PrismaExtendedClient()");
-
-    // Replace any with PrismaExtendedClient
-    fileData = fileData.replace(/any/g, "PrismaExtendedClient");
-    fileData = fileData.replace("keyof PrismaExtendedClient", "keyof typeof PrismaExtendedClient");
-
-    // Save the file
-    fs.writeFileSync(path.join(__dirname, "../src/orm/ORM.ts"), fileData, "utf8");
+    // Replace the value in ORM.ts with the Prisma client.
+    fs.writeFileSync(path.join(__dirname, "../src/orm/ORM.ts"), prismaORMCode, "utf8");
 } catch (e) {
     console.log(e)
 }
--- src/orm/ORM.ts
+++ src/orm/ORM.ts
@@ -1,9 +1,1 @@
-export class ORM {
-    private _orm: any;
-
-    constructor() {
-        this._orm = {};
-    }
-
-    [key: keyof any]: any[keyof any]; 
-}
+export class ORM {}
