e698daee6d4a33f6abd7fb5a7411cc30a27ce985
Irisu <nekomarisakirisame@gmail.com>
2023-04-29 06:20:26 -0400

    agh

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -6,7 +6,7 @@ try {
     
     // Prisma is installed, so we'll use it as an ORM.
     // Replace the value in ORM.ts with the following:
-    const fileData = fs.readFileSync(path.join(__dirname, "../src/orm/ORM.ts", "utf8"));
+    const fileData = fs.readFileSync(path.join(__dirname, "../src/orm/ORM.ts"), "utf8");
 
     // Add the following line to the top of the file:
     // import { PrismaExtendedClient } from "./PrismaExtendedClient.orm.ts";
