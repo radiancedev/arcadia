a38637d031d405caee0ba69ede41c5554b87420c
Irisu <nekomarisakirisame@gmail.com>
2023-04-30 10:54:07 -0400

    dap me up brotha

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,32 +1,12 @@
 const fs = require("fs");
 const path = require("path");
 
-const prismaORMCode = `import { PrismaClient } from "@prisma/client";
-
-export class ORM extends PrismaClient {
-    public omit<T, K extends keyof T>(obj: T | undefined, keys: K[]): Omit<T, K> {
-        if (!obj) {
-            return {} as Omit<T, K>;
-        }
-
-        const ret: Record<keyof T, any> | undefined = obj;
-        for (let key of Object.keys(obj)) {
-            if (keys.includes(key as K)) {
-                delete ret?.[key as keyof T];
-            }
-        }
-
-        return ret;
-    }
-}
-`;
-
 try {
     const _ = require("@prisma/client"); 
     
     // Prisma is installed, so we'll use it as an ORM.
     // Replace the value in ORM.ts with the Prisma client.
-    fs.writeFileSync(path.join(__dirname, "../src/orm/ORM.ts"), prismaORMCode, "utf8");
+    fs.writeFileSync(path.join(__dirname, "../src/orm/ORM.ts"), fs.readFileSync(path.join(__dirname, "../src/orm/ORM.prisma.ts"), "utf-8"), "utf8");
 } catch (e) {
     throw e;
 }
