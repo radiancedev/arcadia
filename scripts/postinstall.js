54c664b40a19f771c1445f08eae2434190f1060e
Irisu <nekomarisakirisame@gmail.com>
2023-04-30 10:56:19 -0400

    forgot to remove the throw statement lol

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -8,5 +8,4 @@ try {
     // Replace the value in ORM.ts with the Prisma client.
     fs.writeFileSync(path.join(__dirname, "../src/orm/ORM.ts"), fs.readFileSync(path.join(__dirname, "../src/orm/ORM.prisma.ts"), "utf-8"), "utf8");
 } catch (e) {
-    throw e;
 }
